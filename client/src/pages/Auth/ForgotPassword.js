bdd86c78089bb46cef800cb3faec80d88032256b
sohail <khansohail33624@gmail.com>
2025-02-04 20:37:09 +0530

    Login Corrected

--- client/src/pages/Auth/ForgotPassword.js
+++ client/src/pages/Auth/ForgotPassword.js
@@ -17,7 +17,7 @@ const ForgotPassword = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const res = await axios.post("http://localhost:4000/api/v1/auth/forgot-password", {
+        const res = await axios.post("/api/v1/auth/forgot-password", {
           email,
           newPassword,
           answer
